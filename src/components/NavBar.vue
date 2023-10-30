7e39b36ea7a45493c9242143915df45a320b3b8f
Robbie Jameson <robbie.jameson.123@gmail.com>
2023-10-30 17:04:27 +1300

    eto project initialised

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,17 +7,18 @@
       <router-link to="/shop" class="navTag">Shop</router-link>
     </nav>
   </div>
-  <transition mode="out-in" name="slide">
-    <div class="nav-collapse" @click="navbarOpen = !navbarOpen">
-      <p>=</p>
+
+  <div class="nav-collapse" @click="handleNavClick">
+    <p>=</p>
+    <transition mode="out-in" name="slide">
       <nav :class="navbarOpen ? 'show-nav' : 'hide-nav'">
         <router-link to="/" class="navTag">Home</router-link>
         <router-link to="/about" class="navTag">About</router-link>
         <router-link to="/hireUs" class="navTag">Hire Us</router-link>
         <router-link to="/shop" class="navTag">Shop</router-link>
       </nav>
-    </div>
-  </transition>
+    </transition>
+  </div>
 </template>
 <script>
 export default {
@@ -27,34 +28,16 @@ export default {
       navbarOpen: false,
     };
   },
+
   methods: {
-    beforeEnter(el) {
-      el.style.transform = "translateX(100%)"; // Initial position for entering
-    },
-    enter(el, done) {
-      el.style.transform = "translateX(0)"; // Final position for entering
-      el.addEventListener("transitionend", () => console.log("enter Transition ended"), done);
-    },
-    beforeLeave(el) {
-      el.style.transform = "translateX(0)"; // Initial position for leaving
-    },
-    leave(el, done) {
-      el.style.transform = "translateX(-100%)"; // Final position for leaving
-      el.addEventListener("transitionend", done);
-      console.log(el);
+    handleNavClick() {
+      this.navbarOpen = !this.navbarOpen;
+      console.log("clicked");
     },
   },
 };
 </script>
 <style scoped lang="scss">
-.slide-enter-active,
-.slide-leave-active {
-  transition: transform 2000ms ease-in-out;
-}
-.slide-enter,
-.slide-leave-to {
-  transform: translateX(100%);
-}
 #nav {
   display: flex;
   padding: 1rem;
@@ -85,6 +68,14 @@ export default {
   }
 
   .nav-collapse {
+    .v-enter-active,
+    .v-leave-active {
+      transition: opacity 3s ease-in-out;
+    }
+    .v-enter-from,
+    .v-leave-to {
+      opacity: 0;
+    }
     position: relative;
     display: flex;
     justify-content: flex-end;
